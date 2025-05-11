f4987e5d356d439a7e9f0d911864f429541c4042
OrcaX <jbl01570@hanmail.net>
2025-05-12 01:16:13 +0900

    Update Message.js

--- orcax-chat-backend/models/Message.js
+++ orcax-chat-backend/models/Message.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  nickname: { type: String, required: true },
-  content: { type: String, required: true },
-  color: { type: String, default: '#333' },
-  type: { type: String, default: 'user' },
-  createdAt: { type: Date, default: Date.now }
+  sender: String,
+  message: String,
+  color: String,
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
