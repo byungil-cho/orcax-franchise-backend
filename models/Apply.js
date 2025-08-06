1bae7fc2ce7f954ba32bb625de5d0f73c3c680f1
OrcaX <jbl01570@hanmail.net>
2025-08-06 20:18:42 +0900

    Add files via upload

--- models/Apply.js
+++ models/Apply.js
@@ -1,1 +1,13 @@
-1
+const mongoose = require('mongoose');
+
+const ApplySchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    bizNo: String,
+    region: String,
+    address: String,
+    category: String,
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Apply', ApplySchema);
